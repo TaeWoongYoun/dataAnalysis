c2d66521637da47b3448ba227b5b60ef7e18b672
TaeWoongYoun <24457545yong@gmail.com>
2025-05-11 21:46:09 +0900

    test : Create csv file

--- testStorage.py
+++ testStorage.py
@@ -1,6 +1,22 @@
 import pandas as pd
 import yfinance as yf
-import datetime as dt
 
-df_tsla = yf.download('TSLA', start='2021-11-01', end=dt.date.today())
-df_tsla.to_excel("./TSLA_OHLCV_TODAY.xlsx")
+tsla = yf.download('TSLA', start='2021-11-01', end='2023-03-31')
+df_tsla = pd.DataFrame(tsla['Close'])
+df_tsla.head()
+df_tsla = df_tsla.reset_index()
+df_tsla.columns = ['date', 'value']
+df_tsla['date'] = pd.to_datetime(df_tsla['date'])
+import matplotlib.pyplot as plt
+
+# 데이터 분할하기
+df_tsla_train = pd.DataFrame(df_tsla['value'][:int(0.8*len(df_tsla))])
+df_tsla_test = pd.DataFrame(df_tsla['value'][int(0.8*len(df_tsla)):])
+df_tsla_train['date'] = df_tsla['date'][:int(0.8*len(df_tsla))]
+df_tsla_test['date'] = df_tsla['date'][int(0.8*len(df_tsla)):]
+df_tsla_train.set_index('date', inplace=True)
+df_tsla_test.set_index('date', inplace=True)
+
+df_tsla_train['value'].plot(figsize=(12.2, 6.4), color='blue')
+df_tsla_test['value'].plot(color='green')
+plt.show()
